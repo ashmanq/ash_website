adbf28e8a2ff4eaf2ec7d0fe5da44dc23fdf3e8b
Ashir <ashirq@outlook.com>
2020-05-25 22:02:49 +0100

    minor updates to app.js

--- server/app.js
+++ server/app.js
@@ -8,6 +8,8 @@ const createRouter = require('./helpers/create_router.js');
 app.use(express.json());
 app.use(cors());
 
+
+// Create a connection to the MySQL database using configuration from config file
 const con = mysql.createConnection({
   host: dbConfig.host,
   user: dbConfig.user,
@@ -15,17 +17,25 @@ const con = mysql.createConnection({
   database: dbConfig.database
 });
 
-
+// Connect to the database and create routes
 con.connect((err) => {
   if(err) throw err;
+
   // We create routers for both coding and drawing projects
   const codingRouter = createRouter(con, 'coding');
   const drawingRouter = createRouter(con, 'drawing');
 
-  // We then allocate these routes to the express app
-  app.use('/api/coding', codingRouter);
-  app.use('/api/drawing', drawingRouter);
-
+  // In production environment the app sits in a subdomain. A check is made for a
+  // production environment URI. If one exists it is appended to the app routes
+  if(process.env.base_uri){
+    // Routes with base_uri appended
+    app.use(process.env.base_uri + '/api/coding', codingRouter);
+    app.use(process.env.base_uri + '/api/drawing', drawingRouter);
+  } else {
+    // Routes without any base_uri
+    app.use('/api/coding', codingRouter);
+    app.use('/api/drawing', drawingRouter);
+  }
   console.log('MySQL Connected...');
 });
 
